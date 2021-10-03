d899943c3f60226965ca3a0cfd3de427daae951e
Carlos Rhedney <twisterpsa@hotmail.com>
2021-10-02 23:01:20 -0300

    adding i forgot my password and class mailer.php

--- vendor/mypets/php-classes/src/Model/User.php
+++ vendor/mypets/php-classes/src/Model/User.php
@@ -3,10 +3,13 @@ namespace Mypets\Model;
 
 use \Mypets\DB\Sql;
 use \Mypets\Model;
+use \Mypets\Mailer;
 
 class User extends Model
 {
 	const SESSION = "User";
+	const SECRET = "TccMypets_Secret";
+	const SECRET_IV = "TccMypets_Secret_IV";
 
 	public static function login($login, $password)
 	{
@@ -121,6 +124,97 @@ class User extends Model
 		$sql = new Sql();
 
 		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>$this->getiduser()
+		));
+	}
+
+	public static function getForgot($mail)
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.mail = :mail", array(
+			":mail"=>$mail
+		));
+
+		if(count($results) === 0)
+		{
+
+			throw new \Exception("Não foi possível recuperar a senha!");
+
+		}else
+		{
+			$data = $results[0];
+
+			$resultsRecovery = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :ip)", array(
+				":iduser"=>$data["iduser"],
+				":ip"=>$_SERVER["REMOTE_ADDR"]
+			));
+
+			if(count($resultsRecovery) === 0)
+			{
+
+				throw new \Exception("Não foi possível recuperar a senha!");
+
+			}else
+			{
+				$dataRecovery = $resultsRecovery[0];
+
+				$cde = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($cde);
+
+				$link = "http://www.mypets.com.br/admin/forgot/reset?code=$code";
+
+				$mailer = new Mailer($data["mail"], $data["person"], "Redefinir senha MyPets", "mail", array(
+					"name"=>$data["person"],
+					"link"=>$link
+				));
+
+				$mailer->send();
+
+				return $data;
+
+			}
+		}
+	}
+
+	public static function validForgotDecrypt($code)
+	{
+		$code = base64_decode($code);
+
+		$idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_userspasswordsrecoveries a INNER JOIN tb_users b USING(iduser) INNER JOIN tb_persons c USING(idperson) WHERE a.idrecovery = :idrecovery AND a.dtrecovery IS NULL and DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW()", array(
+			":idrecovery"=>$idrecovery
+		));
+
+		if(count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha");
+
+		}else
+		{
+			return $results[0];
+		}
+	}
+
+	public static function setForgotUsed($idrecovery)
+	{
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+			":idrecovery"=>$idrecovery
+		));
+	}
+
+	public function setPassword($password)
+	{
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+			":password"=>$password,
 			":iduser"=>$this->getiduser()
 		));
 	}
