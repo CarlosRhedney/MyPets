aa66c0a1ef05e4fd7707b211ad9245dd6265d26e
Carlos Rhedney <twisterpsa@hotmail.com>
2021-12-08 02:34:10 -0300

    Adding details in README.md

---
+++ views-cache/header.7a6ff3285aaa56b58925d39dfc7a1587.rtpl.php
@@ -0,0 +1,140 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="icon" type="image/icon" href="/res/site/img/logo.png">
+  <title>MyPets</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="/res/admin/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/res/admin/dist/css/AdminLTE.min.css">
+
+  <link rel="stylesheet" href="/res/admin/dist/css/skins/skin-purple.min.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition skin-purple sidebar-mini">
+<div class="wrapper">
+
+  <!-- Main Header -->
+  <header class="main-header">
+
+    <a href="/admin" class="logo">
+      <span class="logo-mini"><b>M</b>Ps</span>
+      <span class="logo-lg"><b>My</b>Pets</span>
+    </a>
+
+    <nav class="navbar navbar-static-top" role="navigation">
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="dropdown messages-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-envelope-o"></i>
+              <span class="label label-success">4</span>
+            </a>
+            <ul class="dropdown-menu">
+              <li class="header">4 mensagens</li>
+              <li class="footer"><a href="#">Ver todas</a></li>
+            </ul>
+          </li>
+
+          <li class="dropdown notifications-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-bell-o"></i>
+              <span class="label label-warning">10</span>
+            </a>
+            <ul class="dropdown-menu">
+              <li class="header">10 notificações</li>
+              <li>
+                <ul class="menu">
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-users text-aqua"></i> 5 novos membros
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="footer"><a href="#">Ver todos</a></li>
+            </ul>
+          </li>
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="/res/admin/dist/img/carlos.jpg" class="user-image" alt="User Image">
+              <span class="hidden-xs"><?php echo getUserName(); ?></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li class="user-header">
+                <img src="/res/admin/dist/img/carlos.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  <?php echo getUserName(); ?>
+                  <small>Membro desde jun. 2021</small>
+                </p>
+              </li>
+              <li class="user-body">
+                <div class="row">
+                </div>
+              </li>
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="/profile" class="btn btn-default btn-flat">Minha Conta</a>
+                </div>
+                <div class="pull-right">
+                  <a href="/admin/logout" class="btn btn-default btn-flat">Sair</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <aside class="main-sidebar">
+
+    <section class="sidebar">
+
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="/res/admin/dist/img/carlos.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p><?php echo getUserName(); ?></p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <ul class="sidebar-menu">
+        <li class="header"></li>
+        <li><a href="/admin/users"><i class="fa fa-users"></i> <span>Usuários</span></a></li>
+        <li><a href="/admin/categories"><i class="fa fa-cubes"></i> <span>Categorias</span></a></li>
+        <li><a href="/admin/ongs"><i class="fa fa-institution"></i> <span>Ongs</span></a></li>
+        <li><a href="/admin/pets"><i class="fa fa-paw"></i> <span>Animais</span></a></li>
+        <li class="treeview">
+          <a href="#"><i class="fa fa-folder-open"></i> <span>Relatórios</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="/admin/user/relatory">Usuários</a></li>
+            <li><a href="/admin/pet/relatory">Animais</a></li>
+            <li><a href="/admin/ong/relatory">ONGs</a></li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+  </aside>
